d9c7789cda8cb0d976caae1fbaf18c19e39ca275
Dodie Joo <dodi258@naver.com>
2020-08-27 00:13:08 +0900

    푼 것.
    1. Programmers - 오랜기간 보호한 동물(1) - NOT IN 구문
    2. Programmers - 있는데요 없었습니다 - JOIN, 날짜비교

---
+++ aug26_2020/Programmers-오랜기간보호한동물(1).sql
@@ -0,0 +1,10 @@
+# 문제 출처: https://programmers.co.kr/learn/courses/30/lessons/59044
+
+SELECT `IN`.`NAME`, `IN`.`DATETIME`
+FROM ANIMAL_INS AS `IN` 
+WHERE `IN`.ANIMAL_ID NOT IN (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_OUTS
+)
+ORDER BY `IN`.`DATETIME` LIMIT 3; 
+
---
+++ aug26_2020/Programmers-있는데요없었습니다.sql
@@ -0,0 +1,8 @@
+# 문제 출처: https://programmers.co.kr/learn/courses/30/lessons/59043
+
+SELECT `IN`.ANIMAL_ID, `IN`.`NAME`
+FROM ANIMAL_INS AS `IN`
+JOIN ANIMAL_OUTS AS `OUT` 
+ON `IN`.ANIMAL_ID = `OUT`.ANIMAL_ID
+WHERE `OUT`.`DATETIME` < `IN`.`DATETIME`
+ORDER BY `IN`.`DATETIME`;  
