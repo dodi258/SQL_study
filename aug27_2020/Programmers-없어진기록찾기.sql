06f6e6e7103a0eb62d978e94a01e8e5e980ff891
Dodie Joo <dodi258@naver.com>
2020-08-27 23:05:12 +0900

    푼 것.
    1. Programmers - 루시와 엘라 찾기 - IN
    2. Programmers - 없어진 기록 찾기 - NOT IN
    3. Programmers - el이 들어가는 동물 찾기 - LOWER(), LIKE

---
+++ aug27_2020/Programmers-없어진기록찾기.sql
@@ -0,0 +1,9 @@
+# 문제 출처: https://programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_OUTS
+WHERE ANIMAL_ID NOT IN (
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+)
+ORDER BY ANIMAL_ID; 
---
+++ aug27_2020/Programmers-이름에el이들어가는동물찾기.sql
@@ -0,0 +1,7 @@
+# 문제 출처: https://programmers.co.kr/learn/courses/30/lessons/59047
+
+SELECT ANIMAL_ID, `NAME`
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE='Dog' 
+AND LOWER(`NAME`) LIKE '%el%'
+ORDER BY `NAME`; 
